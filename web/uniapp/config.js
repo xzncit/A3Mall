773d0276d4060b80fc12a8465d362a7a89d0b204
xzncit <158373108@qq.com>
2021-11-29 08:09:33 +0800

    优化更新

--- web/uniapp/config.js
+++ web/uniapp/config.js
@@ -2,8 +2,8 @@ export default {
 	web_name: "A3Mall",
 	//uni_app_web_url: window.location.protocol + "//" + window.location.host+"/",
 	//uni_app_web_api_url: window.location.protocol + "//" + window.location.host+"/api",
-	uni_app_web_url: "http://a3-mall.net/", // h5域名或者PC域名，用于分享图片时使用，结尾必须加 “/”
-	uni_app_web_api_url: "http://a3-mall.net/api", // 请求地址 
+	uni_app_web_url: "http://www.a3-mall.net/", // h5域名或者PC域名，用于分享图片时使用，结尾必须加 “/”
+	uni_app_web_api_url: "http://www.a3-mall.net/api", // 请求地址 
 	web_app_id: "1000",
 	debug: false,
 	
